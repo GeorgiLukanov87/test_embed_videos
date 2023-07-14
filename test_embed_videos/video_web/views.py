bd5293ac938217b3a39c01adf1bb71c6297dc8ef
Georgi St. Lukanov <goshiii87@gmail.com>
2023-07-14 23:46:49 +0200

    testing

--- test_embed_videos/video_web/views.py
+++ test_embed_videos/video_web/views.py
@@ -6,8 +6,13 @@ from test_embed_videos.video_web.models import Video
 def index(request):
     videos = Video.objects.all()
 
+    test1 = Video.objects.all()[0]
+    test2 = Video.objects.all()[1]
+
     context = {
         'videos': videos,
+        'video1': test1,
+        'video2': test2,
     }
 
     return render(request, 'embed_video/index-videos.html', context, )
